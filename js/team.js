da0c887c87109d63e8211c22893ef3d0adeec1ce
DiamoQ <diamond220qwert@mail.ru>
2022-03-30 17:54:38 +0300

    -

--- js/team.js
+++ js/team.js
@@ -3,7 +3,10 @@ let teamList = document.getElementById("teamList");
 const buttons = document.querySelectorAll(".team__name");
 
 
-function closeItems() {
+teamList.addEventListener("click", function (event){
+ event.preventDefault();
+
+ function closeItems() {
   for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
     const description = button.nextElementSibling;
@@ -12,16 +15,12 @@ function closeItems() {
   }
 }
 
-teamList.addEventListener("click", function (event){
- event.preventDefault();
-
  const target = event.target;
  if (target.classList.contains('team__name')){
   const description = target.nextElementSibling;
 
   if (description.classList.contains('team__dropdown--active')){
-    description.classList.remove('team__dropdown--active');
-    target.classList.remove('team__name--active');
+    closeItems()
   } else{
     closeItems();
     description.classList.add('team__dropdown--active');
@@ -29,5 +28,6 @@ teamList.addEventListener("click", function (event){
    }
   }
 });
+
 
 
